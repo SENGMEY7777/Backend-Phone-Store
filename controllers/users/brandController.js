de40ec600b4b0892c53099505b41b1d366e79d44
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-04 01:01:47 +0700

    add brand controller

--- controllers/users/brandController.js
+++ controllers/users/brandController.js
@@ -1,13 +1,51 @@
 const brandService = require('../../services/users/brandService');
+const { sendResponse } = require('../../utils/responeHelper');
 
 const createBrand = async (req, res) => {
     try {
-
+        let body = req.body;
+        let result = await brandService.createBrand(body);
+        return sendResponse(res, 200, true, 'Create brand successfully', result);
     } catch (error) {
-        
+        return sendResponse(res, 400, false, error.message);
     }
 }
 
+const getAllBrand = async (req, res) => {
+    try {
+        let result = await brandService.getAllBrand();
+        return sendResponse(res, 200, true, 'Get all brand successfully', result);
+    }catch (error) {
+        return sendResponse(res, 400, false, error.message);
+    }
+}
+
+const updateBrand = async (req, res) => {
+    try {
+        let body = req.body;
+        let id = req.params.id;
+        let result = await brandService.updateBrand(id, body);
+        return sendResponse(res, 200, true, 'Update brand successfully', result);
+    }catch (error) {
+        return sendResponse(res, 400, false, error.message);
+    }
+}
+
+const deleteBrand = async (req, res) => {
+    try {
+        let body = req.body;
+        let id = req.params.id;
+        let result = await brandService.deleteBrand(id);
+        return sendResponse(res, 200, true, 'Delete brand successfully', result[0]);
+    }catch (error) {
+        return sendResponse(res, 400, false, error.message);
+    }
+}
+
+
 module.exports = {
-    createBrand
+    createBrand,
+    getAllBrand,
+    updateBrand,
+    deleteBrand
 }
