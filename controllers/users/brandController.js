5c2628d4c000a7c511ba2ccca17e9f5d9cc1854f
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-04 00:14:58 +0700

    add brand controller

---
+++ controllers/users/brandController.js
@@ -0,0 +1,13 @@
+const brandService = require('../../services/users/brandService');
+
+const createBrand = async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {
+    createBrand
+}
