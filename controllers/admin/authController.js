cebee18755d9f70b6834223500e9e3b970eb4c88
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-04 00:14:18 +0700

    add admin controller

--- controllers/admin/authController.js
+++ controllers/admin/authController.js
@@ -1,4 +1,15 @@
+const authService = require('../../services/admin/authService');
+const { sendResponse } = require('../../utils/responeHelper');
 
-const register = (req, res) => {
-    
+const login = async (req, res) => {
+    try {
+        let result = await authService.login(req.body);
+        return sendResponse(res, 200, true, 'Admin login successfully', result);
+    } catch (error) {
+        return sendResponse(res, 400, false, error.message);
+    }
+}
+
+module.exports = {
+    login
 }
