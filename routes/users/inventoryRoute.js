a5a9ca2541c08ad032e0e65d7b522367b62af999
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-08 17:09:30 +0700

    add validation invetory log

--- routes/users/inventoryRoute.js
+++ routes/users/inventoryRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const inventoryController = require('../../controllers/users/inventoryController');
 const isLogin = require('../../middlewares/authMiddleware');
+const validator = require('../../middlewares/validate');
+const stockMovementSchema = require('../../validators/users/invetorySchema');
 
-router.post('/create', isLogin, inventoryController.createInventoryLog);
+router.post('/create', validator(stockMovementSchema), isLogin, inventoryController.createInventoryLog);
 router.get('/all', isLogin, inventoryController.getAllInventoryLogs);
 router.get('/product/:productId', isLogin, inventoryController.getInventoryLogsByProductId);
 router.get('/:id', isLogin, inventoryController.getInventoryLogById);
