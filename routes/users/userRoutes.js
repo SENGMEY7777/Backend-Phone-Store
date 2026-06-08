2763dc794e44d4aab402bb14dbaec5e9b380dbaf
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-08 16:57:51 +0700

    add validation in user

--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -2,17 +2,20 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/users/userController');
 const isLogin = require('../../middlewares/authMiddleware');
+const validator = require('../../middlewares/validate');
+const { userSchema, resetPassword, verifyOtpSchema , resetPasswordSchema } = require('../../validators/users/authSchema'); 
+const authSchema = require('../../validators/admin/authAdmin');
 
 
-router.post('/register', userController.register);
-router.post('/login', userController.login);
+router.post('/register', validator(userSchema), userController.register);
+router.post('/login', validator(authSchema), userController.login);
 router.get('/all', isLogin, userController.getAllUser);
 router.delete('/logout', userController.logout);
 router.get('/verify-email', userController.verifyEmail);
 router.post('/resend-verification', userController.resendVerificationEmail);
-router.post('/forgot-password', userController.forgotPassword);
-router.post('/verify-reset-otp', userController.verifyPasswordResetOtp);
-router.post('/reset-password', userController.resetPassword);
+router.post('/forgot-password', validator(resetPassword), userController.forgotPassword);
+router.post('/verify-reset-otp', validator(verifyOtpSchema), userController.verifyPasswordResetOtp);
+router.post('/reset-password', validator(resetPasswordSchema), userController.resetPassword);
 
 
 module.exports = router;
