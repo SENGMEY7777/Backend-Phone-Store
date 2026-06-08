07af0eeef546a4868cb6b31757be044046fe62a3
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-08 17:04:40 +0700

    add validation on branch

--- routes/users/brandRoute.js
+++ routes/users/brandRoute.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const brandController = require('../../controllers/users/brandController');
 const isLogin = require('../../middlewares/authMiddleware');
+const validator = require('../../middlewares/validate');
+const { brandSchema } = require('../../validators/users/brandSchema'); 
 
 router.get('/all', isLogin, brandController.getAllBrand);
-router.post('/create', isLogin, brandController.createBrand);
-router.put('/:id', isLogin, brandController.updateBrand);
+router.post('/create', validator(brandSchema), isLogin, brandController.createBrand);
+router.put('/:id', validator(brandSchema), isLogin, brandController.updateBrand);
 router.delete('/:id', isLogin, brandController.deleteBrand);
 
 
