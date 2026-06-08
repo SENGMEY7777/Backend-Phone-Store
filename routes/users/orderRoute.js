10522113bbc05b9f08042fb64050e3695d6edc34
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-08 18:17:06 +0700

    add validation order

--- routes/users/orderRoute.js
+++ routes/users/orderRoute.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const orderController = require('../../controllers/users/orderController');
 const isLogin = require('../../middlewares/authMiddleware');
+const valdator = require('../../middlewares/validate');
+const orderSchema = require('../../validators/users/orderSchema'); 
 
-router.post('/create', isLogin, orderController.createOrder);
+router.post('/create', valdator(orderSchema), isLogin, orderController.createOrder);
 router.get('/all', isLogin, orderController.getAllOrders);
 router.get('/:id', isLogin, orderController.getOrderById);
-router.put('/:id/status', isLogin, orderController.updateOrderStatus);
+router.put('/:id/status', valdator(orderSchema), isLogin, orderController.updateOrderStatus);
 router.delete('/:id', isLogin, orderController.deleteOrder);
 
 module.exports = router;
