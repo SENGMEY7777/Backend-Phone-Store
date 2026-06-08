225cbe7b50fc6f7d0af09c16c3602e5161a2136f
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-08 16:36:28 +0700

    modify validator

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,6 +1,14 @@
-const {sendResponse} = require('../utils/responseHelper');
+const {sendResponse} = require('../utils/responeHelper');
 
 const validate = (schema) => (req, res, next) =>{
+    if(!schema || typeof schema.validate !== 'function'){
+        return sendResponse(res, 500, false, 'Validation schema is missing or invalid');
+    }
+
+    if(req.body && req.body.password_hash && !req.body.password){
+        req.body.password = req.body.password_hash;
+    }
+
     const {error, value} = schema.validate(req.body,{
         abortEarly: false,
         stripUnknown: true
