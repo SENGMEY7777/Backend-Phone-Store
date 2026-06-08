6052a8174c6f234105054e7c8ca4ac35b9976cb2
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-08 18:31:54 +0700

    add validation review product

--- routes/users/reviewRoute.js
+++ routes/users/reviewRoute.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const reviewController = require('../../controllers/users/reviewController');
 const isLogin = require('../../middlewares/authMiddleware');
+const valdator = require('../../middlewares/validate');
+const  reviewSchema = require('../../validators/users/reviewProductSchema');
 
-router.post('/create', isLogin, reviewController.createReview);
+router.post('/create', valdator(reviewSchema), isLogin, reviewController.createReview);
 router.get('/all', isLogin, reviewController.getAllReviews);
 router.get('/product/:productId', isLogin, reviewController.getReviewsByProductId);
 router.get('/:id', isLogin, reviewController.getReviewById);
-router.put('/:id', isLogin, reviewController.updateReview);
+router.put('/:id', valdator(reviewSchema), isLogin, reviewController.updateReview);
 router.delete('/:id', isLogin, reviewController.deleteReview);
 
 module.exports = router;
