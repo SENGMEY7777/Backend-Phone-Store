1ad247114dc50ca50609030d60d27d0e98c25605
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-08 18:26:11 +0700

    add validation product

--- routes/users/productRoute.js
+++ routes/users/productRoute.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const productController = require('../../controllers/users/productController');
 const { uploadProductImage } = require('../../middlewares/upload');
 const isLogin = require('../../middlewares/authMiddleware');
+const valdator = require('../../middlewares/validate');
+const productSchema = require('../../validators/users/productSchema');
 
-router.post('/create', isLogin, uploadProductImage, productController.createProduct);
+router.post('/create', valdator(productSchema), isLogin, uploadProductImage, productController.createProduct);
 router.get('/all', isLogin, productController.getAllProducts);
 router.get('/:id', isLogin, productController.getProductById);
-router.put('/:id', isLogin, uploadProductImage, productController.updateProduct);
+router.put('/:id', valdator(productSchema), isLogin, uploadProductImage, productController.updateProduct);
 router.delete('/:id', isLogin, productController.deleteProduct);
 
 
